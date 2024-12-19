590a6df4be887e83378e22db07a4e34304c33c3f
Rasel Chowdhury <rasel.chowdhury0001@gmail.com>
2024-12-19 18:01:55 +0600

    Feature section ui done

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,9 +1,11 @@
+import Featured from "./featured/Featured"
 import Hero from "./hero/Hero"
 
 const Home = () => {
     return (
       <>
         <Hero />
+        <Featured/>
       </>
     )
   }
---
+++ src/components/pages/home/featured/Featured.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+import "./Featured.css"
+import FeaturedCard from "./FeaturedCard"
+import Heading from "@/components/shared/Header"
+
+const Featured = () => {
+  return (
+    <>
+      <section className='featured background'>
+        <div className='container'>
+          <Heading title='Featured Property Types' subtitle='Find All Type of Property.' />
+          <FeaturedCard />
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Featured
